8db3bf0ed4c3e38ac2e311c4b55d55066ea41f54
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2019-11-28 00:20:13 +0200

    Add files via upload

---
+++ Countdown.py
@@ -0,0 +1,34 @@
+import time
+from win10toast import ToastNotifier
+import datetime
+
+toast = ToastNotifier()
+
+date_format = "%d-%m-%Y %H:%M:%S"
+paiva = datetime.date.today().strftime("%d-%m-%Y")
+kello = time.strftime("%H:%M:%S")
+aika = paiva + " " + kello
+
+time2  = datetime.datetime.strptime('11-04-2020 15:00:00', date_format)
+time1  = datetime.datetime.strptime(aika, date_format)
+
+diff = time2 - time1
+
+days = diff.days
+#print (str(days) + ' päivää')
+
+hours = int((diff.seconds) / 3600)
+#print('{0} tuntia'.format(tunnit)
+
+minutes = int(((diff.seconds) / 60) % 60)
+#print (str(minutes) + ' Minutes')
+
+seconds = diff.seconds % 60
+#print (str(seconds) + ' secs')
+
+toast.show_toast("Overwatch League Paris 2020",
+                   "{0} päivää, {1} tuntia, {2} minuuttia, {3} sekuntia".format(days, hours, minutes, seconds),
+                   icon_path="owl.ico",
+                   duration=25)
+
+exit(0)
